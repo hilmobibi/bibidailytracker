ac71016c1be5f4ea2f53df4909e74b24a09bf52c
hilmobibi <hilmotomo@gmail.com>
2025-07-28 21:40:20 +0700

    responsive add task button

---
+++ script.js
@@ -0,0 +1,14 @@
+//add task button
+const addTaskBtn = document.getElementById('addTask');
+const menu = document.getElementById('menu');
+
+  addTaskBtn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+  });
+
+  // Optional: Close menu if you click outside
+  document.addEventListener('click', (e) => {
+    if (!menu.contains(e.target) && e.target !== addTaskBtn) {
+      menu.classList.add('hidden');
+    }
+  });
