a5fb272da0f76edf530438492ada04da8ec8d019
hilmobibi <hilmotomo@gmail.com>
2025-08-03 15:11:49 +0700

    bisa kasih nama task

--- script.js
+++ script.js
@@ -21,40 +21,18 @@ deleteTaskBtn.addEventListener('click', () => {
 })
 
 
-//scrollable timeline const list
-const hour00 = document.getElementById('hour00');
-const hour01 = document.getElementById('hour01');
-const hour02 = document.getElementById('hour02');
-const hour03 = document.getElementById('hour03');
-const hour04 = document.getElementById('hour04');
-const hour05 = document.getElementById('hour05');
-const hour06 = document.getElementById('hour06');
-const hour07 = document.getElementById('hour07');
-const hour08 = document.getElementById('hour08');
-const hour09 = document.getElementById('hour09');
-const hour10 = document.getElementById('hour10');
-const hour11 = document.getElementById('hour11');
-const hour12 = document.getElementById('hour12');
-const hour13 = document.getElementById('hour13');
-const hour14 = document.getElementById('hour14');
-const hour15 = document.getElementById('hour15');
-const hour16 = document.getElementById('hour16');
-const hour17 = document.getElementById('hour17');
-const hour18 = document.getElementById('hour18');
-const hour19 = document.getElementById('hour19');
-const hour20 = document.getElementById('hour20');
-const hour21 = document.getElementById('hour21');
-const hour22 = document.getElementById('hour22');
-const hour23 = document.getElementById('hour23');
 
 //apply add task
 function showResult() {
   const select_start_time = document.getElementById('selectStartTime');
   const select_end_time = document.getElementById('selectEndTime');
+  const nameTask = document.getElementById('taskName');
   const startValue = parseInt(select_start_time.value); // convert to number
   const endValue = parseInt(select_end_time.value);
+  const nameValue = nameTask.value;
 
-  if (isNaN(startValue) || isNaN(endValue)) {
+
+  if (isNaN(startValue) || isNaN(endValue) || nameValue === "") {
     alert('isi kontol');
   } else {
     console.log(startValue, endValue);
@@ -75,7 +53,7 @@ function showResult() {
     // Create the task element
     const task = document.createElement("div");
     task.classList.add("task");
-    task.innerText = `Task: ${startValue} - ${endValue}`;
+    task.innerText = `${startValue} - ${endValue} ${nameValue}`;
     task.style.position = "absolute";
     task.style.left = "10vw";
     task.style.top = `calc(${top}vw + 9px)`;
